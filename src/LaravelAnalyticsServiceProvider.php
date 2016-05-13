bb16cd5203483742379cc87b4c7266b850b12abe
Sang Nguyen <sangnguyenplus@gmail.com>
2016-05-12 23:41:36 -0400

    Fix function check missing .p12 file

--- src/LaravelAnalyticsServiceProvider.php
+++ src/LaravelAnalyticsServiceProvider.php
@@ -61,7 +61,7 @@ class LaravelAnalyticsServiceProvider extends ServiceProvider
      */
     protected function guardAgainstMissingP12()
     {
-        if ($this->app['files']->exists(config('laravel-analytics.certificatePath'))) {
+        if (!$this->app['files']->exists(config('laravel-analytics.certificatePath'))) {
             throw new \Exception("Can't find the .p12 certificate in: ".config('laravel-analytics.certificatePath'));
         }
     }
