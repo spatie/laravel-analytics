88eb75beadcd8dade2f3ee2423f3716253b2104d
freek <freek@spatie.be>
2016-06-16 20:12:51 +0200

    add period test

---
+++ tests/PeriodTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Carbon\Carbon;
+use Spatie\Analytics\Period;
+
+class PeriodTest extends PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function it_can_create_a_period_for_a_given_amount_of_days()
+    {
+        Carbon::setTestNow(Carbon::create(2016, 1, 1));
+
+        $period = Period::createForNumberOfDays(10);
+
+        $this->assertSame('2016-01-01', $period->endDate->format('Y-m-d'));
+        $this->assertSame('2015-12-22', $period->startDate->format('Y-m-d'));
+    }
+}
