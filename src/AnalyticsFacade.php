0a2a1fdab020708c9e40065a36418fa8a05af7f4
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-06-20 15:46:27 +0200

    Code review

--- src/AnalyticsFacade.php
+++ src/AnalyticsFacade.php
@@ -4,6 +4,9 @@ namespace Spatie\Analytics;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @see \Spatie\Analytics\Analytics
+ */
 class AnalyticsFacade extends Facade
 {
     protected static function getFacadeAccessor()
