e5ef934ece6c01fa5646bf2d848824f54cc8dbac
Mansoor Khan <8mansoorkhan@gmail.com>
2022-04-30 16:10:44 +0500

    Feature: Convert PHPUnit tests to PestPHP

--- tests/Unit/PeriodTest.php
+++ tests/Unit/PeriodTest.php
@@ -1,82 +1,61 @@
 <?php
 
-namespace Spatie\Analytics\Tests;
-
 use Carbon\Carbon;
-use DateTimeImmutable;
-use PHPUnit\Framework\TestCase;
-use Spatie\Analytics\Exceptions\InvalidPeriod;
 use Spatie\Analytics\Period;
+use Spatie\Analytics\Exceptions\InvalidPeriod;
 
-class PeriodTest extends TestCase
-{
-    /** @test */
-    public function it_can_create_a_period_for_a_given_amount_of_days()
-    {
-        Carbon::setTestNow(Carbon::create(2016, 1, 1));
+it('can_create_a_period_for_a_given_amount_of_days', function () {
+    Carbon::setTestNow(Carbon::create(2016, 1, 1));
 
-        $period = Period::days(10);
+    $period = Period::days(10);
 
-        $this->assertSame('2015-12-22', $period->startDate->format('Y-m-d'));
-        $this->assertSame('2016-01-01', $period->endDate->format('Y-m-d'));
-    }
+    expect($period->startDate->format('Y-m-d'))->toBe('2015-12-22');
+    expect($period->endDate->format('Y-m-d'))->toBe('2016-01-01');
+});
 
-    /** @test */
-    public function it_can_create_a_period_for_a_given_amount_of_months()
-    {
-        Carbon::setTestNow(Carbon::create(2016, 1, 10));
+it('can_create_a_period_for_a_given_amount_of_months', function () {
+    Carbon::setTestNow(Carbon::create(2016, 1, 10));
 
-        $period = Period::months(10);
+    $period = Period::months(10);
 
-        $this->assertSame('2015-03-10', $period->startDate->format('Y-m-d'));
-        $this->assertSame('2016-01-10', $period->endDate->format('Y-m-d'));
-    }
+    expect($period->startDate->format('Y-m-d'))->toBe('2015-03-10');
+    expect($period->endDate->format('Y-m-d'))->toBe('2016-01-10');
+});
 
-    /** @test */
-    public function it_can_create_a_period_for_a_given_amount_of_years()
-    {
-        Carbon::setTestNow(Carbon::create(2016, 1, 12));
+it('can_create_a_period_for_a_given_amount_of_years', function () {
+    Carbon::setTestNow(Carbon::create(2016, 1, 12));
 
-        $period = Period::years(2);
+    $period = Period::years(2);
 
-        $this->assertSame('2014-01-12', $period->startDate->format('Y-m-d'));
-        $this->assertSame('2016-01-12', $period->endDate->format('Y-m-d'));
-    }
+    expect($period->startDate->format('Y-m-d'))->tobe('2014-01-12');
+    expect($period->endDate->format('Y-m-d'))->tobe('2016-01-12');
+});
 
-    /** @test */
-    public function it_provides_a_create_method()
-    {
-        $startDate = Carbon::create(2015, 12, 22);
-        $endDate = Carbon::create(2016, 1, 1);
+it('provides_a_create_method', function () {
+    $startDate = Carbon::create(2015, 12, 22);
+    $endDate = Carbon::create(2016, 1, 1);
 
-        $period = Period::create($startDate, $endDate);
+    $period = Period::create($startDate, $endDate);
 
-        $this->assertSame('2015-12-22', $period->startDate->format('Y-m-d'));
-        $this->assertSame('2016-01-01', $period->endDate->format('Y-m-d'));
-    }
+    expect($period->startDate)->toBe($startDate);
+    expect($period->endDate)->toBe($endDate);
+});
 
-    /** @test */
-    public function it_accepts_datetime_immutable_instances()
-    {
-        $startDate = Carbon::create(2015, 12, 22)->toIso8601String();
-        $startDateImmutable = new DateTimeImmutable($startDate);
-        $endDate = Carbon::create(2016, 1, 1)->toIso8601String();
-        $endDateImmutable = new DateTimeImmutable($endDate);
+it('accepts_datetime_immutable_instances', function () {
+    $startDate = Carbon::create(2015, 12, 22)->toIso8601String();
+    $startDateImmutable = new DateTimeImmutable($startDate);
+    $endDate = Carbon::create(2016, 1, 1)->toIso8601String();
+    $endDateImmutable = new DateTimeImmutable($endDate);
 
-        $period = Period::create($startDateImmutable, $endDateImmutable);
+    $period = Period::create($startDateImmutable, $endDateImmutable);
 
-        $this->assertSame('2015-12-22', $period->startDate->format('Y-m-d'));
-        $this->assertSame('2016-01-01', $period->endDate->format('Y-m-d'));
-    }
+    expect($period->startDate->format('Y-m-d'))->toBe('2015-12-22');
+    expect($period->endDate->format('Y-m-d'))->toBe('2016-01-01');
+});
 
-    /** @test */
-    public function it_will_throw_an_exception_if_the_start_date_comes_after_the_end_date()
-    {
-        $startDate = Carbon::create(2016, 1, 1);
-        $endDate = Carbon::create(2015, 1, 1);
+it('will_throw_an_exception_if_the_start_date_comes_after_the_end_date', function () {
+    $startDate = Carbon::create(2016, 1, 1);
+    $endDate = Carbon::create(2015, 1, 1);
 
-        $this->expectException(InvalidPeriod::class);
-
-        Period::create($startDate, $endDate);
-    }
-}
+    Period::create($startDate, $endDate);
+})->throws(InvalidPeriod::class);
