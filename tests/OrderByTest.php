858b7710500f69da395ef7e7c2540c4d220056f0
timvandijck <timvandijck@users.noreply.github.com>
2023-03-31 13:52:51 +0000

    Fix styling

--- tests/OrderByTest.php
+++ tests/OrderByTest.php
@@ -1,21 +1,20 @@
 <?php
 
-
-use Spatie\Analytics\OrderBy;
 use Google\Analytics\Data\V1beta\OrderBy as GoogleOrderBy;
+use Spatie\Analytics\OrderBy;
 
 it('should create GoogleOrderBy objects for dimensions', function () {
-   $result = OrderBy::dimension('dimension', true);
+    $result = OrderBy::dimension('dimension', true);
 
-   expect(get_class($result))->toBe(GoogleOrderBy::class)
-       ->and($result->getDimension()->getDimensionName())->toBe('dimension')
-       ->and($result->getDesc())->toBeTrue();
+    expect(get_class($result))->toBe(GoogleOrderBy::class)
+        ->and($result->getDimension()->getDimensionName())->toBe('dimension')
+        ->and($result->getDesc())->toBeTrue();
 });
 
 it('should create GoogleOrderBy objects for metrics', function () {
-   $result = OrderBy::metric('metric', true);
+    $result = OrderBy::metric('metric', true);
 
-   expect(get_class($result))->toBe(GoogleOrderBy::class)
-       ->and($result->getMetric()->getMetricName())->toBe('metric')
-       ->and($result->getDesc())->toBeTrue();
+    expect(get_class($result))->toBe(GoogleOrderBy::class)
+        ->and($result->getMetric()->getMetricName())->toBe('metric')
+        ->and($result->getDesc())->toBeTrue();
 });
